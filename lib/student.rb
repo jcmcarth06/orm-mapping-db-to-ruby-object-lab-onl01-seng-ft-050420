b22a891a6b7a2389b5f4eeb23b63733bd5ec1c5e
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 22:13:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,9 @@ class Student
     FROM students
     WHERE grade < 12
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.drop_table
