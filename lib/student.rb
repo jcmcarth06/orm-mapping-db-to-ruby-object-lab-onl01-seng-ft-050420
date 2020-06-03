3d595a2214027f3605ed7252593054bedb231b44
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 22:14:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.all_students_below_12th_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
     Select *
     FROM students
