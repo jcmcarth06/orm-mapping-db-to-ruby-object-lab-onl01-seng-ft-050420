d39e8498a0e83634030d9fc135a3615aa8b251fc
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 22:11:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.all_students_belo
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+    Select *
+    FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
