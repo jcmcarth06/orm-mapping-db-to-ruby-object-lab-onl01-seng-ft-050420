b396cd4676f8a8a00beef75fd3d8258a8968638e
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 21:58:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,13 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student = self.new  
+    student = self.new
     student.id = row[0]
     student.name =  row[1]
     student.grade = row[2]
-    student  
+    student
   end
-    
+
 
   def self.all
     sql = <<-SQL
@@ -26,20 +26,20 @@ class Student
       WHERE name = ?
       LIMIT 1
     SQL
- 
+
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
